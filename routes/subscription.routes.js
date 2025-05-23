18ddd228c4586cf40d78ffc47d393859416d0a01
Noureddine Kechroud <nouroukechroud@icloud.com>
2025-05-23 14:44:28 +0100

    Create routes folder and subscription routes GET, POST, PUT, DELETE initial methods.

---
+++ routes/subscription.routes.js
@@ -0,0 +1,22 @@
+import {Router} from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', (req, res) => res.send({title: 'GET all subscription'}));
+
+subscriptionRouter.post('/', (req, res) => res.send({title: 'POST a new subscription'}));
+
+subscriptionRouter.get('/:id', (req, res) => res.send({title: 'GET a subscription by id'}));
+
+subscriptionRouter.put('/:id', (req, res) => res.send({title: 'PUT a subscription by id'}));
+
+subscriptionRouter.delete('/:id', (req, res) => res.send({title: 'DELETE a subscription by id'}));
+
+subscriptionRouter.get('/user/:id', (req, res) => res.send({title: 'GET all of a user subscription'}));
+
+subscriptionRouter.put('/:id/cancel', (req, res) => res.send({title: 'CANCEL a user subscription'}));
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({title: 'GET upcoming renewals'}));
+
+
+export default subscriptionRouter;
