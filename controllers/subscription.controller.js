fd759f6206cd0d5492315fe94d35ecd0114b2821
Noureddine Kechroud <nouroukechroud@icloud.com>
2025-05-24 16:32:38 +0100

    Subscription feature: create getAllSubscriptions & getSubscription by id functions to handle the GET method routes for getting either subs by id or all subs.

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -12,3 +12,49 @@ export const createSubscription = async (req, res, next) => {
         next(e);
     }
 }
+
+export const getUserSubscriptions = async (req, res, next) => {
+    try {
+        if (req.user._id !== req.params.id) {
+            const error = new Error('You\'re not the not the owner of this account.' );
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({user: req.params.id});
+        res.status(200).json({ success: true, data: subscriptions });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const getAllSubscriptions = async (req, res, next) => {
+    try {
+        const subscriptons = await Subscription.find();
+        if (!subscriptons) {
+            const error = new Error('There is no one subscriptions');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        res.status(200).json({ success: true, data: subscriptons });
+
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const getSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+        if (!subscription) {
+            const error = new Error('Subscription does not exist');
+            error.statusCode = 401;
+            throw error;
+        }
+        res.status(200).json({ success: true, data: subscription });
+
+    } catch (e) {
+        next(e);
+    }
+}
