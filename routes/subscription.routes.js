d4b60f025316752a5ab842cef1d81fe925c6c9b8
Noureddine Kechroud <nouroukechroud@icloud.com>
2025-05-24 16:30:38 +0100

    Subscription feature: update GET routes for getting all subscriptions with admin middleware & getAllSubscriptions controller. Also update GET Method for get subscription by id with auth middleware & getSubscription controller.

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,20 +1,26 @@
 import {Router} from "express";
 import authorize from "../middlewares/auth.middleware.js";
-import {createSubscription} from "../controllers/subscription.controller.js";
+import {
+    createSubscription,
+    getAllSubscriptions,
+    getSubscription,
+    getUserSubscriptions
+} from "../controllers/subscription.controller.js";
+import admin from "../middlewares/admin.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => res.send({title: 'GET all subscription'}));
+subscriptionRouter.get('/', admin, getAllSubscriptions);
 
 subscriptionRouter.post('/', authorize, createSubscription);
 
-subscriptionRouter.get('/:id', (req, res) => res.send({title: 'GET a subscription by id'}));
+subscriptionRouter.get('/:id', authorize, getSubscription);
 
 subscriptionRouter.put('/:id', (req, res) => res.send({title: 'PUT a subscription by id'}));
 
 subscriptionRouter.delete('/:id', (req, res) => res.send({title: 'DELETE a subscription by id'}));
 
-subscriptionRouter.get('/user/:id', (req, res) => res.send({title: 'GET all of a user subscription'}));
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
 subscriptionRouter.put('/:id/cancel', (req, res) => res.send({title: 'CANCEL a user subscription'}));
 
