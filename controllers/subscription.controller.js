b33ac7e079b7e6c90fd32bafb97dc06645b99fe3
Noureddine Kechroud <nouroukechroud@icloud.com>
2025-05-26 12:33:34 +0100

    Subscription feature: create upcoming renewals function to display to user upcoming renwals.

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -36,9 +36,7 @@ export const getAllSubscriptions = async (req, res, next) => {
     try {
         const subscriptons = await Subscription.find();
         if (!subscriptons) {
-            const error = new Error('There is no one subscriptions');
-            error.statusCode = 401;
-            throw error;
+            res.status(404).json({ success: false, message: 'Subscriptions not found.' });
         }
 
         res.status(200).json({ success: true, data: subscriptons });
@@ -52,9 +50,7 @@ export const getSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.findById(req.params.id);
         if (!subscription) {
-            const error = new Error('Subscription does not exist');
-            error.statusCode = 404;
-            throw error;
+            res.status(404).json({ message: 'Subscription not found.' });
         }
 
         res.status(200).json({ success: true, data: subscription });
@@ -68,9 +64,7 @@ export const cancelSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.findById(req.params.id);
         if(!subscription) {
-            const error = new Error('Subscription does not exist');
-            error.statusCode = 404;
-            throw error;
+            res.status(404).json({ success: false, message: 'Subscription not found.' });
         }
 
         if (subscription.user.data !== req.user.id.data) {
@@ -99,6 +93,26 @@ export const updateSubscription = async (req, res, next) => {
             }, {new: true, runValidators: true});
 
         if(!subscription) {
+            res.status(404).json({ success: false, message: 'Subscription not found.' });
+        }
+
+        if (subscription.user.data !== req.user.id.data) {
+            const error = new Error('You are not an admin of this account to update the subscription.' );
+            error.statusCode = 401;
+            throw error;
+        }
+
+        res.status(200).json({ success: true, message: 'Subscription updated successfully.', data: {subscription} });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const deleteSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findByIdAndDelete(req.params.id);
+
+        if(!subscription) {
             const error = new Error('Subscription does not exist');
             error.statusCode = 404;
             throw error;
@@ -110,7 +124,37 @@ export const updateSubscription = async (req, res, next) => {
             throw error;
         }
 
-        res.status(200).json({ success: true, message: 'Subscription updated successfully.', data: {subscription} });
+        res.status(200).json({ success: true, message: 'Subscription deleted successfully.', data: {subscription} });
+
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const upcomingRenewals = async (req, res, next) => {
+    try {
+
+        if (req.user.role !== 'admin' && req.user.id !== req.params.id) {
+            const error = new Error('You are not authorized to access this data.');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        const sevenDays = 8 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
+        const sevenDaysLater = new Date(Date.now() + sevenDays);
+
+
+        const subscriptions = await Subscription.find({
+            user: req.params.id,
+            renewalDate: { $lte: sevenDaysLater },
+        });
+
+        if (!subscriptions || subscriptions.length === 0) {
+            res.status(404).json({ success: false, message: 'You have no upcoming subscriptions.' });
+        }
+
+        res.status(200).json({ success: true, count: subscriptions.length, data: subscriptions });
+
     } catch (e) {
         next(e);
     }
